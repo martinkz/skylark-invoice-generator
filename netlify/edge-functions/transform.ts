b30092071e4943fb907cc181d6b88d0efb4a7ec5
Martin K <martinkz@users.noreply.github.com>
2022-12-06 13:21:41 +0000

    Improve error handling

--- netlify/edge-functions/transform.ts
+++ netlify/edge-functions/transform.ts
@@ -3,14 +3,23 @@ import { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
 
-  // Look for the query parameter, and return if we don't find it
+  const response = await context.next();
+  let text = await response.text();
+
   if (url.searchParams.get("id") === null) {
-    return;
+    return new Response(null, response);
   }
 
-  let invoiceName = atob(url.searchParams.get("id"));
+  let invoiceName;
 
-  context.log(`Transforming the response from this ${url} for name: ${invoiceName}`);
+  try {
+    invoiceName = atob(url.searchParams.get("id"));
+  } catch(e) {
+    console.log(e);
+    return new Response('<!doctype html><html><body>Invoice ID not found.</body></html>', response);
+  }
+
+  context.log(`Transforming ${url} for name: ${invoiceName}`);
 
   const jsonResponse = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1oQronS_Sq679Oy1DAXL3A7xgDb0MrYbixRx8m6Su08o/values/2022-2023-T1?key=AIzaSyAsaZWn7_HcXpR69f_wKRaSX1dQXwwlOxg");
   const data = await jsonResponse.json();
@@ -18,17 +27,9 @@ export default async (request: Request, context: Context) => {
   const columnNames = data.values.shift();
   const tokenNames = columnNames.map(item => `{{${item}}}`);
   const invoiceData = data.values;
-
-  // console.log(invoiceData);
-
   let currentInvoice = invoiceData.find(item => item.includes(invoiceName));
 
   currentInvoice.length = currentInvoice.length - 3; // Remove the last 3 columns, as they're not used
-
-  // console.log(columnNames, currentInvoice);
-
-  const response = await context.next();
-  let text = await response.text();
 
   currentInvoice.forEach( (val,idx) =>  text = text.replaceAll(tokenNames[idx], val) );
 
