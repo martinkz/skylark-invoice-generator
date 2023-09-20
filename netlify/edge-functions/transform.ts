e78c038a7949679a671d3ded75a3f67fda3b58ae
Martin <martinkz@users.noreply.github.com>
2023-09-20 18:42:00 +0100

    Rename variables

--- netlify/edge-functions/transform.ts
+++ netlify/edge-functions/transform.ts
@@ -4,16 +4,16 @@ export default async (request: Request, context: Context) => {
   const url = new URL(request.url);
 
   const response = await context.next();
-  let text = await response.text();
+  let indexHtmlText = await response.text();
 
   if (url.searchParams.get("id") === null) {
     return new Response(null, response);
   }
 
-  let invoiceName: string;
+  let invoiceNo: string;
 
   try {
-    invoiceName = atob(url.searchParams.get("id")!);
+		invoiceNo = atob(url.searchParams.get("id")!);
   } catch(e) {
     console.log(e);
     return new Response('<!doctype html><html><body>Invoice ID not found.</body></html>', response);
@@ -22,7 +22,7 @@ export default async (request: Request, context: Context) => {
   let sheetId: string = Deno.env.get("GOOGLE_SHEET_ID");
   let sheetKey: string = Deno.env.get("GOOGLE_SHEET_KEY");
 
-  context.log(`Transforming ${url} for name: ${invoiceName}`);
+  context.log(`Transforming ${url} for name: ${invoiceNo}`);
 
   // Get all sheet names: https://sheets.googleapis.com/v4/spreadsheets/${sheetId}?fields=sheets%2Fproperties%2Ftitle&key=${sheetKey}
   // From https://stackoverflow.com/questions/55018655/get-all-data-of-multiple-worksheet-in-google-api-in-js
@@ -32,11 +32,11 @@ export default async (request: Request, context: Context) => {
   const columnNames = data.values.shift();
   const tokenNames = columnNames.map((item: string) => `{{${item}}}`);
   const invoiceData = data.values;
-  let currentInvoice = invoiceData.find((item: string) => item.includes(invoiceName));
+  let currentInvoice = invoiceData.find((item: string) => item.includes(invoiceNo));
 
   currentInvoice.length = currentInvoice.length - 3; // Remove the last 3 columns, as they're not used
 
-  currentInvoice.forEach( (val: string, idx: string) =>  text = text.replaceAll(tokenNames[idx], val) );
+  currentInvoice.forEach((val: string, idx: string) => (indexHtmlText = indexHtmlText.replaceAll(tokenNames[idx], val)));
 
-  return new Response(text, response);
+  return new Response(indexHtmlText, response);
 };
